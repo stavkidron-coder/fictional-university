194d79db3698081fc0a17bfbcda3e76ebe2ca90d
Stav Kidron <kidronstav@gmail.com>
2021-02-04 23:11:49 -0600

    Added in webpack and json files with node

--- functions.php
+++ functions.php
@@ -1,13 +1,19 @@
 <?php
 
     function university_files(){
-        wp_enqueue_script('main-university-javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
-
         wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-
         wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        
+        if (strstr($_SERVER['SERVER_NAME'], 'http://amazing-college.local/')) {
+            wp_enqueue_script('main-university-javascript', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+        } else {
+            wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+            wp_enqueue_script('main-university-javascript', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
+            wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
+        }
+        
+        
 
-        wp_enqueue_style('university_main_styles', get_stylesheet_uri());
     }
 
     add_action('wp_enqueue_scripts', 'university_files');
